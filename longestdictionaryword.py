913d0400393895896b2875965199862bc6e0442a
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-06 14:00:10 -0500

    Updated from Brython Server: 12/6/2017 2:00:09 PM

--- longestdictionaryword.py
+++ longestdictionaryword.py
@@ -5,9 +5,8 @@
 dictionary = open('engmix.txt')
 
 wordcount = 0
-for word in dictionary:
-    longest = ''
-    for w in word:
-        if len(w)>len(longest):
-            longest = word
-print('The longest word is',longest)
+for w in dictionary:
+    m = ' '
+    if len(w)>len(m):
+        m = w
+print('The longest word is',m)
