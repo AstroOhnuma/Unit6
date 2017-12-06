9aee5f362451fefc93d802f1083ee3c389afd1dd
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-06 13:46:58 -0500

    Updated from Brython Server: 12/6/2017 1:46:58 PM

--- filedemo.py
+++ filedemo.py
@@ -2,4 +2,13 @@
 #12/6/17
 #filedemo.py - how to read a file
 
-open('engmix.txt')
+dictionary = open('engmix.txt')
+
+wordcount = 0
+for word in dictionary:
+    if 'astro' in word:
+        print(word.strip())
+    wordcount += 1
+    
+print('There are', wordcount,'words in the dictionary.')
+
