f15147debf293cda0189420c08dbc3b75d0f8feb
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-07 14:54:29 -0500

    Updated from Brython Server: 12/7/2017 2:54:28 PM

--- reversefile.py
+++ reversefile.py
@@ -1,1 +1,11 @@
+#Astro Ohnuma
+#12/7/17
+#reversefile.py - asking the user for the name of a file and printing out all of the lines of the file in reverse order
 
+flipped = input('Enter the name of a file: ')
+file = open(flipped)
+word = []
+for line in file:
+    word.append(line)
+word.reverse()
+print(word)
