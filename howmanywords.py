d3d78cfa6100d085f63d278dd051809c58e0197a
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-07 14:32:37 -0500

    Updated from Brython Server: 12/7/2017 2:32:36 PM

--- howmanywords.py
+++ howmanywords.py
@@ -3,5 +3,7 @@
 #howmanywords.py - prints out how many words there are for each number of letters in the dictionary
 
 dictionary = open('engmix.txt')
-
-
+words = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+for word in dictionary:
+    words[len(word)-1] = words[len(word)-1]+1
+print(words)
