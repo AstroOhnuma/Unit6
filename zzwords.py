2c13e517763f752e04419e7dbfbff1f71fa12880
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-07 14:09:40 -0500

    Updated from Brython Server: 12/7/2017 2:09:39 PM

--- zzwords.py
+++ zzwords.py
@@ -1,1 +1,11 @@
+#Astro Ohnuma
+#12/7/17
+#zzwords.py - print out all the words in the dictionary that contain "zz"
+
+dictionary = open('engmix.txt')
+
+for word in dictionary:
+    if 'astro' in word:
+        print(word.strip())
+
 
