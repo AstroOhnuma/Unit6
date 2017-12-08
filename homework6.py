a92a3479770ebdd6a91daa3a95d544f6afdd04a1
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-08 14:31:52 -0500

    Updated from Brython Server: 12/8/2017 2:31:50 PM

--- homework6.py
+++ homework6.py
@@ -1,1 +1,19 @@
+#Astro Ohnuma
+#12/7/17
+#homework6.py - 6th computer science homework assignment
 
+dictionary = open('engmix.txt')
+search = input('Enter a word to search for in the dictionary: ')
+wordcount = 0
+for word in dictionary:
+    if search in word:
+        inside = True
+    else:
+        inside = False
+if inside == True:
+    print(search,'is in the dictionary.')
+elif inside == False:
+    print(search,'is not in the dictionary.')
+
+        
+    
