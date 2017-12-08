6b988560de666d6c7b9760c85214f5ae480184ab
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-08 14:44:10 -0500

    Updated from Brython Server: 12/8/2017 2:44:09 PM

--- homework6.py
+++ homework6.py
@@ -2,6 +2,8 @@
 #12/7/17
 #homework6.py - 6th computer science homework assignment
 
+#1.
+'''
 dictionary = open('engmix.txt')
 search = input('Enter a word to search for in the dictionary: ')
 inside = False
@@ -12,6 +14,14 @@ if inside == True:
     print(search,'is in the dictionary.')
 elif inside == False:
     print(search,'is not in the dictionary.')
+'''
+#2.
+dictionary = open('engmix.txt')
+words = []
+for line in dictionary:
+    words.append(line)
+num = int(input('Enter a number: '))
+print(words[num-1])
 
         
     
