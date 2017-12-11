8c9e280b32655d97e315fb6fb9ea6be8f04c8260
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-11 14:26:19 -0500

    Updated from Brython Server: 12/11/2017 2:26:18 PM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,9 @@
+#Astro Ohnuma
+#12/11/17
+#warmup16.py - Finding all the words in the dictionary that start with your first initial and end with your last initial
 
+dictionary = open('engmix.txt')
+
+for word in dictionary:
+    if word[0] == 'a' and word[-1] == 'o':
+        print(word.strip())
