f1591a8ade210bacdf6ccce75a833a7519512161
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-13 13:31:24 -0500

    Updated from Brython Server: 12/13/2017 1:31:24 PM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,11 @@
+#Astro Ohnuma
+#12/13/17
+#quiz6.py - quiz on files
 
+# Program 1
+dictionary = open('engmix.txt')
+three = 'c'
+two = 'p'
+for word in dictionary:
+    if word.count(three) == 3 and word.count(two) == 2:
+        print(word.strip())
