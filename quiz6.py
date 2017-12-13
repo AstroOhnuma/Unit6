becd69085e893d733f9c923df7c1116d5f176175
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-12-13 13:34:06 -0500

    Updated from Brython Server: 12/13/2017 1:34:05 PM

--- quiz6.py
+++ quiz6.py
@@ -3,9 +3,17 @@
 #quiz6.py - quiz on files
 
 # Program 1
+'''
 dictionary = open('engmix.txt')
 three = 'c'
 two = 'p'
 for word in dictionary:
     if word.count(three) == 3 and word.count(two) == 2:
-        print(word.strip())
+        print(word.strip())'''
+# Program 2
+dictionary = open('engmix.txt')
+for word in dictionary:
+    word = word.strip()
+    if word != '':
+        if word[0] == 'r':
+            print(word.strip())
